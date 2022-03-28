8c9e838470f214729800d40772ec9dc28202d0af
chenh <641760512@qq.com>
2022-03-28 11:10:47 +0800

    testng断言及日志信息

---
+++ 06.ExtendReport/src/main/java/com/tester/extent/demo/TestMethodsDemo.java
@@ -0,0 +1,33 @@
+package com.tester.extent.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+/**
+ * @author chenpi
+ * @create 2022-03-28 10:53
+ */
+public class TestMethodsDemo {
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(1, 2);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals("abc", "abc");
+    }
+
+    @Test
+    public void logDemo() {
+        Reporter.log("自定义日志");
+        throw new RuntimeException("自定义异常信息");
+    }
+}
