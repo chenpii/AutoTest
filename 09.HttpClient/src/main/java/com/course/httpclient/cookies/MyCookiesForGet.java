3b0f3a73a9b34935256b14a041356c6fbf0aff5b
chenh <641760512@qq.com>
2022-03-30 14:36:48 +0800

    http返回结果转json

--- 09.HttpClient/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
+++ 09.HttpClient/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -1,10 +1,15 @@
 package com.course.httpclient.cookies;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -37,6 +42,11 @@ public class MyCookiesForGet {
         HttpClient httpClient = new DefaultHttpClient();
         HttpResponse response = httpClient.execute(httpGet);
         result = EntityUtils.toString(response.getEntity());
-        System.out.println(result);
+        JSONObject jsonObject = JSON.parseObject(result);
+        Assert.assertEquals(jsonObject.getString("huhansan"),"success");
+        Assert.assertEquals(jsonObject.getInteger("status"),1);
+
     }
+
+
 }
