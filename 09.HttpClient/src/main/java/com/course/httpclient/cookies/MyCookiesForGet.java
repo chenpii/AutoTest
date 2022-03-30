7426349e66b88368343de2b65a7de4552bf12528
chenh <641760512@qq.com>
2022-03-30 11:18:25 +0800

    配置优化：从properties中读取URL

---
+++ 09.HttpClient/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -0,0 +1,42 @@
+package com.course.httpclient.cookies;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+/**
+ * @author chenpi
+ * @create 2022-03-29 16:54
+ */
+public class MyCookiesForGet {
+    private String url;
+    private ResourceBundle bundle;
+
+    @BeforeTest
+    public void beforeTest() {
+        bundle = ResourceBundle.getBundle("application");
+        url = bundle.getString("test.url");
+
+    }
+
+    @Test
+    public void test() throws IOException {
+        //从配置文件中获取URL
+        String uri = bundle.getString("getCookies.uri");
+        String fullUrl = url + uri;
+        String result;
+
+        HttpGet httpGet = new HttpGet(fullUrl);
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpResponse response = httpClient.execute(httpGet);
+        result = EntityUtils.toString(response.getEntity());
+        System.out.println(result);
+    }
+}
--- 09.HttpClient/src/main/java/com/course/httpclient/demo/MyHttpClient.java
+++ 09.HttpClient/src/main/java/com/course/httpclient/demo/MyHttpClient.java
@@ -1,4 +1,4 @@
-package com.chenpi.httpclient.demo;
+package com.course.httpclient.demo;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
