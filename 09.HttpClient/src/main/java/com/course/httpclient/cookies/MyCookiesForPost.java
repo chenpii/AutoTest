875089e4a98ffdd94b5099a2724efae755610baa
chenh <641760512@qq.com>
2022-03-31 15:19:24 +0800

    post请求及json断言

--- 09.HttpClient/src/main/java/com/course/httpclient/cookies/MyCookiesForPost.java
+++ 09.HttpClient/src/main/java/com/course/httpclient/cookies/MyCookiesForPost.java
@@ -1,11 +1,12 @@
 package com.course.httpclient.cookies;
 
-
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
-
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
@@ -18,14 +19,12 @@ import java.util.ResourceBundle;
 
 /**
  * @author chenpi
- * @create 2022-03-29 16:54
+ * @create 2022-03-31 11:03
  */
-public class MyCookiesForGet {
+public class MyCookiesForPost {
     private String url;
     private ResourceBundle bundle;
-    //用来保存cookies
     private CookieStore store;
-
 
     @BeforeTest
     public void beforeTest() {
@@ -62,28 +61,38 @@ public class MyCookiesForGet {
     }
 
     @Test(dependsOnMethods = "testGetCookies")
-    public void testGetWithCookies() throws IOException {
+    public void testPostWithCookies() throws IOException {
         //从配置文件中获取URL
-        String uri = bundle.getString("getWithCookies.uri");
+        String uri = bundle.getString("postWithCookies.uri");
         String fullUrl = this.url + uri;
         String result;
-        HttpGet httpGet = new HttpGet(fullUrl);
+        HttpPost post = new HttpPost(fullUrl);
         DefaultHttpClient httpClient = new DefaultHttpClient();
 
-        //设置cookies
+        //设置请求头信息 设置header
+        post.setHeader("content-type", "application/json");
+
+        //设置json参数
+        JSONObject param = new JSONObject();
+        param.put("name", "huhansan");
+        param.put("age", "18");
+        //将参数信息添加到方法中
+        StringEntity entity = new StringEntity(param.toString(), "utf-8");
+        post.setEntity(entity);
+        //设置cookies信息
         httpClient.setCookieStore(this.store);
-
-        //执行get请求
-        HttpResponse response = httpClient.execute(httpGet);
-
-        //获取响应状态码
-        int statusCode = response.getStatusLine().getStatusCode();
-        Assert.assertEquals(statusCode, 200);
-
+        //执行post请求
+        HttpResponse response = httpClient.execute(post);
+        //断言状态
+        Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+        //获取结果
         result = EntityUtils.toString(response.getEntity());
-        System.out.println(result);
+        //将返回的响应结果字符串转化成Json
+        JSONObject jsonResult = JSONObject.parseObject(result);
+        //断言json字段
+        Assert.assertEquals(jsonResult.getString("huhansan"), "success");
+        Assert.assertEquals(jsonResult.getInteger("status"), 1);
 
     }
-
 
 }
