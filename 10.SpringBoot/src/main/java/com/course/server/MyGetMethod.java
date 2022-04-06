cc5307c78fcbcf112f9eb974f8fb751140dfced9
chenh <641760512@qq.com>
2022-04-06 10:09:26 +0800

    携带Cookies才能访问的get接口

--- 10.SpringBoot/src/main/java/com/course/server/MyGetMethod.java
+++ 10.SpringBoot/src/main/java/com/course/server/MyGetMethod.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 /**
  * RestController:告诉ComponentScan，我是需要被你扫描的类
@@ -27,5 +29,28 @@ public class MyGetMethod {
         Cookie cookie = new Cookie("login", "true");
         response.addCookie(cookie);
         return "恭喜你获得Cookies成功";
+    }
+
+    /**
+     * 要求客户端携带Cookies访问
+     */
+    @RequestMapping(value = "/getWithCookies", method = RequestMethod.GET)
+    public String getWithCookies(HttpServletRequest request) {
+
+        Cookie[] cookies = request.getCookies();
+        if (Objects.isNull(cookies)) {
+            return "cookies为空，需携带Cookies信息";
+        }
+
+        for (Cookie c : cookies
+        ) {
+            if (c.getName().equals("login") && c.getValue().equals("true")) {
+                return "这是一个需要携带Cookies信息才能访问的get接口";
+            }
+
+        }
+
+        return "未携带Cookies信息";
+
     }
 }
