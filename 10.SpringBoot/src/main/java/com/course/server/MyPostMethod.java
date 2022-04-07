7f646bb8901fda714a09f8efa336287299cd5282
chenh <641760512@qq.com>
2022-04-07 10:50:52 +0800

    Cookies验证和返回用户列表的POST接口开发

--- 10.SpringBoot/src/main/java/com/course/server/MyPostMethod.java
+++ 10.SpringBoot/src/main/java/com/course/server/MyPostMethod.java
@@ -3,12 +3,10 @@ package com.course.server;
 import com.course.bean.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -25,6 +23,14 @@ public class MyPostMethod {
 
     //用户登录成功获取到Cookies，然后再访问其他接口获取列表
 
+    /**
+     * 登陆接口
+     *
+     * @param response
+     * @param userName 用户名
+     * @param passWord 密码
+     * @return
+     */
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ApiOperation(value = "登录接口，成功后获取Cookies信息", httpMethod = "POST")
     public String login(HttpServletResponse response,
@@ -40,9 +46,26 @@ public class MyPostMethod {
         throw new RuntimeException("用户密码错误，登录失败！");
     }
 
-@RequestMapping(value = "/getUser")
-    public User getUsers() {
-        User user = new User();
-        return user;
+
+    @RequestMapping(value = "/getUserList", method = RequestMethod.POST)
+    @ApiOperation(value = "获取用户列表", httpMethod = "POST")
+    public User getUsers(HttpServletRequest request,
+                         HttpServletResponse response,
+                         @RequestBody User u) {
+
+        //获取Cookies
+        Cookie[] cookies = request.getCookies();
+        for (Cookie c : cookies) {
+            if ("login".equals(c.getName())
+                    && "true".equals(c.getValue())
+                    && "root".equals(u.getUserName())
+                    && "310012".equals(u.getPassWord())) {
+
+                User user = new User("tom", 23, "man");
+                return user;
+            }
+        }
+        throw new RuntimeException("参数不合法");
+
     }
 }
