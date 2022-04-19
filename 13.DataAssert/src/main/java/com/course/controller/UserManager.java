b57433fa90cbaa75a7c4219bed209778baf9e189
chenh <641760512@qq.com>
2022-04-19 15:54:15 +0800

    login API

--- 13.DataAssert/src/main/java/com/course/controller/UserManager.java
+++ 13.DataAssert/src/main/java/com/course/controller/UserManager.java
@@ -1,11 +1,16 @@
 package com.course.controller;
 
+import com.course.model.User;
 import io.swagger.annotations.Api;
 import lombok.extern.log4j.Log4j;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author chenpi
@@ -18,4 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserManager {
     @Autowired
     private SqlSessionTemplate template;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public boolean login(HttpServletResponse response,
+                         @RequestBody User user) {
+
+        return true;
+    }
+
 }
