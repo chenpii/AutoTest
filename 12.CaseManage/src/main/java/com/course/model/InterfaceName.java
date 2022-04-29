d965ca0a3dbc98d5f1073b7bf18c0b0f2d80a8f0
chenpi <641760512@qq.com>
2022-04-29 16:00:23 +0800

    优化

--- 12.CaseManage/src/main/java/com/course/model/InterfaceName.java
+++ 12.CaseManage/src/main/java/com/course/model/InterfaceName.java
@@ -2,10 +2,15 @@ package com.course.model;
 
 /**
  * 接口名称，防止乱传
+ *
  * @author chenpi
  * @create 2022-04-09 14:32
  */
 public enum InterfaceName {
-    GETUSERLIST, LOGIN, UPDATEUSERINFO, GETUSERINFO, ADDUSER
+    LOGIN,
+    ADDUSER,
+    UPDATEUSERINFO,
+    GETUSERLIST,
+    GETUSERINFO
 
 }
